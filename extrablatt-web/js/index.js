3a945abc8b5d9a5369659951e83ecaa2466f2f57
mattsse <matthias.seitz@tum.de>
2020-08-22 16:32:52 +0200

    feat: add webpage example

--- extrablatt-web/js/index.js
+++ extrablatt-web/js/index.js
@@ -1,3 +1,5 @@
+import {reqwest_article} from "../pkg";
+
 import("../pkg/index.js").catch(console.error);
 
 document.getElementById("btn-extract").addEventListener("click", extract, false);
@@ -16,15 +18,16 @@ function setUrlFromLocation() {
 
 async function extract() {
     const url = document.getElementById("url-to-extract").value;
-    // check url
-    // fetch news
-
-    clearTable();
-
-    const table = generateTable({url: "dummy"});
-
-    document.getElementById("extract-root").appendChild(table);
-    return false;
+    if (url) {
+        clearTable();
+        try {
+            const article = await reqwest_article(url);
+            const table = generateTable(url, article);
+            document.getElementById("extract-root").appendChild(table);
+        } catch (e) {
+            setError(e, url);
+        }
+    }
 }
 
 function clearTable() {
@@ -34,19 +37,169 @@ function clearTable() {
     }
 }
 
-function generateTable(data) {
+function setError(err, url) {
+    const tableRoot = document.createElement("div");
+    tableRoot.setAttribute("id", "extract-tableRoot");
+    tableRoot.innerHTML = "<h3>" + err + "</h3>";
+    tableRoot.innerHTML += "<p>" + url + "</p>"
+}
+
+function generateTable(url, article) {
     const tableRoot = document.createElement("div");
     tableRoot.setAttribute("id", "extract-tableRoot");
     // set title
-    tableRoot.innerHTML = "<h3>" + "Extracted data" + "</h3>";
-    tableRoot.innerHTML += "<p>" + data.url + "</p>"
+    const title = document.createElement("h3");
+    title.innerText = "Extracted Data";
+    tableRoot.appendChild(title);
+    tableRoot.appendChild(createLinkCaption(url))
 
     const table = document.createElement("table");
+    table.setAttribute("class", "table table-striped table-bordered");
+
+    const tBody = table.createTBody();
+    createTextValueRow(tBody.insertRow(), "Title", article.title);
+
+    let authors = article.authors;
+    if (authors) {
+        authors = authors.join(", ")
+    }
+    createTextValueRow(tBody.insertRow(), "Authors", authors);
+
+    let published = article.publishing_date;
+    let updated = null;
+    if (published) {
+        updated = published.last_updated;
+        published = published.published.DateTime;
+        if (updated) {
+            updated = updated.DateTime;
+        }
+    }
+    console.log(JSON.stringify(published));
+    console.log(JSON.stringify(updated));
+    createTextValueRow(tBody.insertRow(), "Published", published);
+    createTextValueRow(tBody.insertRow(), "Updated", updated);
+
+    let keywords = article.keywords;
+    if (authors) {
+        keywords = keywords.join(", ")
+    }
+    createTextValueRow(tBody.insertRow(), "Keywords", keywords);
+
+    createTextValueRow(tBody.insertRow(), "Description", article.description);
+
+    createImageRow(tBody.insertRow(), "Top Image", article.top_image)
+
+    createArticleTextRow(tBody.insertRow(), article.text);
+
+    createReferences(tBody.insertRow(), article.references);
+
+    createImages(tBody.insertRow(), article.images);
+
+    createVideos(tBody.insertRow(), article.videos);
+
     tableRoot.appendChild(table);
-
-    let thead = table.createTHead();
-
     return tableRoot
+}
+
+function createReferences(row, references) {
+    let cell = row.insertCell();
+    let text = document.createTextNode("References");
+    cell.appendChild(text);
+    cell = row.insertCell();
+    if (references) {
+        for (let i = 0; i < references.length; i++) {
+            const caption = createLinkCaption(references[i]);
+            cell.appendChild(caption);
+        }
+    }
+}
+
+function createVideos(row, videos) {
+    let cell = row.insertCell();
+    let text = document.createTextNode("Videos");
+    cell.appendChild(text);
+    cell = row.insertCell();
+    if (videos) {
+        for (let i = 0; i < videos.length; i++) {
+            const caption = createLinkCaption(videos[i]);
+            cell.appendChild(caption);
+        }
+    }
+}
+
+function createImages(row, images) {
+    let cell = row.insertCell();
+    let text = document.createTextNode("Images");
+    cell.appendChild(text);
+    cell = row.insertCell();
+    if (images) {
+        for (let i = 0; i < images.length; i++) {
+            const val = createImageAndCaption(images[i]);
+            cell.appendChild(val.img);
+            cell.appendChild(val.caption);
+        }
+    }
+}
+
+function createArticleTextRow(row, txt) {
+    let cell = row.insertCell();
+    let text = document.createTextNode("Text");
+    cell.appendChild(text);
+    cell = row.insertCell();
+    if (txt) {
+        const lines = txt.split(/\n/);
+
+        console.log(lines.length);
+
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
+            if (line) {
+                const p = document.createElement("p");
+                p.innerText = line;
+                cell.appendChild(p);
+            }
+        }
+    }
+}
+
+function createTextValueRow(row, name, value) {
+    let cell = row.insertCell();
+    let text = document.createTextNode(name);
+    cell.appendChild(text);
+    cell = row.insertCell();
+    if (value) {
+        text = document.createTextNode(value);
+        cell.appendChild(text);
+    }
+}
+
+function createImageAndCaption(url) {
+    const img = document.createElement("img");
+    img.setAttribute("src", url);
+    const caption = createLinkCaption(url);
+    caption.setAttribute("class", "text-center");
+    return {img, caption}
+}
+
+function createImageRow(row, name, url) {
+    let cell = row.insertCell();
+    const text = document.createTextNode(name);
+    cell.appendChild(text);
+    cell = row.insertCell();
+    if (url) {
+        const val = createImageAndCaption(url);
+        cell.appendChild(val.img);
+        cell.appendChild(val.caption);
+    }
+}
+
+function createLinkCaption(url) {
+    const caption = document.createElement("p");
+    const link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.innerText = url;
+    caption.appendChild(link);
+    return caption;
 }
 
 
--- extrablatt-web/src/lib.rs
+++ extrablatt-web/src/lib.rs
@@ -1,6 +1,9 @@
+use extrablatt::Article;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
 use web_sys::console;
-
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,18 +13,41 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen]
+pub async fn fetch_article(url: String) -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
 
-// This is like the `main` function, except for JavaScript.
-#[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
+    console::log_1(&JsValue::from_str(&format!("requesting {}", url)));
 
+    let request = Request::new_with_str_and_init(&url, &opts)?;
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    request.headers().set("Accept", "*/*")?;
 
-    Ok(())
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let txt = JsFuture::from(resp.text()?).await?;
+
+    let doc = txt
+        .as_string()
+        .ok_or_else(|| JsValue::from_str("Received empty response"))?;
+
+    let article = Article::new(&url, doc).map_err(|err| JsValue::from_str(&format!("{}", err)))?;
+
+    JsValue::from_serde(&article.content).map_err(|err| JsValue::from_str(&format!("{}", err)))
+}
+
+#[wasm_bindgen]
+pub async fn reqwest_article(url: String) -> Result<JsValue, JsValue> {
+    let article = Article::content(&url)
+        .await
+        .map_err(|err| JsValue::from_str(&format!("{}", err)))?;
+    JsValue::from_serde(&article).map_err(|err| JsValue::from_str(&format!("{}", err)))
 }
